c21e765b01551e364fbc2c142bac735adb4331eb
zbdtc <13588047212@163.com>
2017-10-11 22:47:01 +0800

    22222

--- config_override.py
+++ config_override.py
@@ -1,11 +1,7 @@
 configs = {
     'db': {
 	    'host': 'localhost',
-<<<<<<< HEAD
 	    'password': '12345678',
-=======
-	    'password': '*********',
->>>>>>> 62c6f214009ee690b5af582e5e8709459943f90e
 	    'database': 'awesome'
     },
 }
